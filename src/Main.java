d63d11d325effc72085a1c6d883cd3c4d9c38bcd
michaelmenzies <michaelam2027@student.cis.edu.hk>
2024-08-30 12:27:02 +0800

    Update Main.java
    Included academic honesty pledge.

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your age:");
+    int age = scanner.nextInt();
+    scanner.nextLine();
+
+    System.out.println("Enter you first name:");
+    String name = scanner.nextLine();
+
+    System.out.println("Enter your favourite food: ");
+    String food = scanner.nextLine();
+
+    System.out.println("\nFirst Name: " + name + "\nAge: " + name + "\nFavourite Food:" + food);
+
+
 
 
 
